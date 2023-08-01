d8686f2c3efcf3efe58e8a1ae3b640e2a6bf2f8f
Mellow1213 <dpcks7282@gmail.com>
2023-08-01 21:42:03 +0900

    Temp UI Gold Text / TMPro Import

---
+++ Assets/02. Scripts/Manager/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI goldText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goldText.text = "Current Gold\n"+GameManager.Instance.gold.ToString();
+    }
+}
