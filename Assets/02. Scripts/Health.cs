cb8adc71f624515c57c45d78b033945e2cfabc24
Mellow1213 <dpcks7282@gmail.com>
2023-08-01 19:55:27 +0900

    적 처치 시 Gold 드랍되도록 변경
    Health.cs 스크립트에 작성
    플레이어 tag가 달린 객체가 사망할 때에는 Gold가 드랍되지 않음

--- Assets/02. Scripts/Health.cs
+++ Assets/02. Scripts/Health.cs
@@ -6,12 +6,15 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float health;
     public GameObject deathEffect;
+    public int dropGold;
     public void Damage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
             Debug.Log(transform.name + " 객체의 HP가 0 이하가 되어 사망합니다.");
+            if(!gameObject.CompareTag("Player"))
+                GameManager.Instance.gold += dropGold;
             Instantiate(deathEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
